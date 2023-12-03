90e843b4e39a16bb5cf1546ce0da9115ea2a1e8c
ChristianCeT <chrisatnat@hotmail.com>
2023-12-02 19:43:50 -0500

    so close finish

--- public/data/invitated-people.ts
+++ public/data/invitated-people.ts
@@ -49,7 +49,6 @@ export const InvitatedPeople: People[] = [
   {
     name: "jose1",
     guest: "Jose Cervantes",
-    family: "Cervantes",
     message:
       "¡El amor está en el aire y nos complace compartir este momento especial contigo! Te invitamos cordialmente a celebrar el día más emocionante de nuestras vidas. Únete a nosotros mientras intercambiamos nuestros votos y sellamos nuestro amor en el sagrado vínculo del matrimonio. Será un día lleno de alegría, risas y momentos inolvidables. ¡Esperamos contar con tu presencia para hacer de este día algo aún más especial!",
   },
